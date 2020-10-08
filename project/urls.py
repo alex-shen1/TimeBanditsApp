33530f52ad99b6a78aa77f830ca21cab673a61fe
Nick <nju3km@virginia.edu>
2020-10-08 16:03:20 -0400

    google login added

--- project/urls.py
+++ project/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name="social_app/index.html")),
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path('accounts/', include('allauth.urls')),
 ]
