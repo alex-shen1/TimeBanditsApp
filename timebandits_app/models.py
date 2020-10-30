920b6f2ef05713b6487c9e47e8aeaa7feb3faf47
Dylan Fernandes <dylan.fernandes00@gmail.com>
2020-10-30 15:00:52 -0400

    added more styling

--- timebandits_app/models.py
+++ timebandits_app/models.py
@@ -6,7 +6,7 @@ from django.db import models
 # from django.core import validators
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
-
+from django import forms
 
 # from django.contrib.postgres.fields import ArrayField
 # from django.utils import timezone
