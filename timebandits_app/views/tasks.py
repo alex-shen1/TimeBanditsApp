53106abad019a372e6971a70c0b89a7bd3790bda
Nick <nju3km@virginia.edu>
2020-11-05 14:57:13 -0500

    Stripe fully implemented

--- timebandits_app/views/tasks.py
+++ timebandits_app/views/tasks.py
@@ -4,10 +4,7 @@ from django.http import HttpResponseRedirect
 from django.views import generic
 from django.shortcuts import render, get_object_or_404
 from django.conf import settings
-from django.views.decorators.csrf import csrf_exempt
-from django.http import JsonResponse
 import stripe
-import json
 
 from ..models import Task
 from ..forms.task_form import TaskForm
@@ -36,7 +33,7 @@ def create_task(request):
         if form.is_valid():
             form.save(commit=True)
             if form.cleaned_data['donation_amount'] > 0:
-                return render(request, 'tasks/checkout.html', {'form': form})
+                return render(request, 'tasks/checkout.html', {'form': form, 'amount': form.cleaned_data['donation_amount']})
             return HttpResponseRedirect('/tasks')
     else:
         form = TaskForm()
@@ -66,32 +63,17 @@ def delete_task(request, pk):
     obj.delete()
     return HttpResponseRedirect('/tasks')
 
-@csrf_exempt
-def createpayment(request):
+def charge(request):
     """Pays the donation amount of a task"""
-    stripe.api_key = 'sk_test_51Hi2a6DLHK87XPh2eZS2T6z5TzsOQ9nsWUWfxyKUtYXNGPM5HWQYL70bK0S7y9kL13i2VGwkvcwjmqGdiu56Znib00S9IYDOZl'
     if request.method == 'POST':
-        form = TaskForm(request.POST)
-        charge = stripe.PaymentIntent.create(
-            amount=500,
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        charge = stripe.Charge.create(
+            amount=int(float(request.POST.get("amount",""))*100),
             currency='usd',
-            metadata={'integration_check': 'accept_a_payment'},
+            description='Donations are mandatory, refunds are optional',
+            source=request.POST['stripeToken']
             )
-        try:
-            return JsonResponse({'publishableKey': 'your test publishable key', 'clientSecret': charge.client_secret})
-        except Exception as e:
-            return JsonResponse({'error':str(e)},status= 403)
-
-def paymentcomplete(request):
-	#if request.method=="POST":
-		#data = json.loads(request.POST.get("payload"))
-    return HttpResponseRedirect('/tasks')
-
-def get_context_data(self, **kwargs):
-    """Sets the key for the payment"""
-    context = super().get_context_data(**kwargs)
-    context['key'] = settings.STRIPE_PUBLISHABLE_KEY
-    return context
+        return HttpResponseRedirect('/tasks')
 
 class TaskDetailsView(generic.DetailView):
     """Displays details for a particular Task."""
