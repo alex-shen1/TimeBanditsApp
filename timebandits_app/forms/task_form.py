f103e0947bb3c7d7f4dcfca15cea660277126b3e
Alex Shen <shen.alexander@gmail.com>
2020-11-06 13:53:29 -0500

    Pylint fixes

--- timebandits_app/forms/task_form.py
+++ timebandits_app/forms/task_form.py
@@ -6,14 +6,14 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = {
-        "owner",
-        "task_title",
-        "task_description",
-        "task_capacity",
-        "event_date",
-        "time_to_complete",
-        "donation_amount",
-        "event_address"}
+            "owner",
+            "task_title",
+            "task_description",
+            "task_capacity",
+            "event_date",
+            "time_to_complete",
+            "donation_amount",
+            "event_address"}
         widgets = {
             'task_description': forms.Textarea(),
         }
--- timebandits_app/tests/tests.py
+++ timebandits_app/tests/tests.py
@@ -1,8 +1,11 @@
 """Tests for the site"""
+# pylint: disable=imported-auth-user
+
 from django.test import TestCase
-# from timebandits_app.models import Task
-from timebandits_app.forms.task_form import TaskForm
 from django.contrib.auth.models import User
+
+from timebandits_app.forms.task_form import TaskForm
+
 
 class TrivialTest(TestCase):
     """Contains trivial tests"""
@@ -26,10 +29,11 @@ class TaskTest(TestCase):
 
     def test_invalid_capacity(self):
         """Tests that tasks with negative capacity fails."""
-        self.user = User.objects.create_user(username='testuser', password='12345')
+        user = User.objects.create_user(
+            username='testuser', password='12345')
         form_data = self.form_data_template.copy()
         form_data["task_capacity"] = "-1"
-        form_data["owner"] = self.user.account
+        form_data["owner"] = user.account
         form = TaskForm(form_data)
         self.assertEqual(
             form.errors['task_capacity'],
@@ -38,20 +42,22 @@ class TaskTest(TestCase):
 
     def test_valid_capacity(self):
         """Tests that tasks with positive capacity pass."""
-        self.user = User.objects.create_user(username='testuser', password='12345')
+        user = User.objects.create_user(
+            username='testuser', password='12345')
         form_data = self.form_data_template.copy()
         form_data["task_capacity"] = "1"
-        form_data["owner"] = self.user.account
+        form_data["owner"] = user.account
         form = TaskForm(form_data)
         print(form.errors)
         self.assertTrue(form.is_valid())
 
     def test_invalid_date(self):
         """Tests that tasks with date in the past fail."""
-        self.user = User.objects.create_user(username='testuser', password='12345')
+        user = User.objects.create_user(
+            username='testuser', password='12345')
         form_data = self.form_data_template.copy()
         form_data["event_date"] = "2020-08-08"
-        form_data["owner"] = self.user.account
+        form_data["owner"] = user.account
         form = TaskForm(form_data)
         self.assertEqual(
             form.errors['event_date'],
@@ -59,19 +65,21 @@ class TaskTest(TestCase):
 
     def test_valid_date(self):
         """Tests that tasks with valid dates pass."""
-        self.user = User.objects.create_user(username='testuser', password='12345')
+        user = User.objects.create_user(
+            username='testuser', password='12345')
         form_data = self.form_data_template.copy()
         form_data["event_date"] = "2021-09-09"
-        form_data["owner"] = self.user.account
+        form_data["owner"] = user.account
         form = TaskForm(form_data)
         self.assertTrue(form.is_valid())
 
     def test_invalid_time_to_complete(self):
         """Tests that tasks with negative time_to_complete fail."""
-        self.user = User.objects.create_user(username='testuser', password='12345')
+        user = User.objects.create_user(
+            username='testuser', password='12345')
         form_data = self.form_data_template.copy()
         form_data["time_to_complete"] = "-1"
-        form_data["owner"] = self.user.account
+        form_data["owner"] = user.account
         form = TaskForm(form_data)
         self.assertEqual(
             form.errors['time_to_complete'],
@@ -79,19 +87,21 @@ class TaskTest(TestCase):
 
     def test_valid_time_to_complete(self):
         """Tests that tasks with valid time_to_complete pass."""
-        self.user = User.objects.create_user(username='testuser', password='12345')
+        user = User.objects.create_user(
+            username='testuser', password='12345')
         form_data = self.form_data_template.copy()
         form_data["time_to_complete"] = "1"
-        form_data["owner"] = self.user.account
+        form_data["owner"] = user.account
         form = TaskForm(form_data)
         self.assertTrue(form.is_valid())
 
     def test_too_high_donation(self):
         """Tests that tasks with donations over $200 maximum fail."""
-        self.user = User.objects.create_user(username='testuser', password='12345')
+        user = User.objects.create_user(
+            username='testuser', password='12345')
         form_data = self.form_data_template.copy()
         form_data["donation_amount"] = "1001"
-        form_data["owner"] = self.user.account
+        form_data["owner"] = user.account
         form = TaskForm(form_data)
         self.assertEqual(
             form.errors['donation_amount'],
@@ -99,10 +109,11 @@ class TaskTest(TestCase):
 
     def test_too_low_donation(self):
         """Tests that tasks with donations over $200 maximum fail."""
-        self.user = User.objects.create_user(username='testuser', password='12345')
+        user = User.objects.create_user(
+            username='testuser', password='12345')
         form_data = self.form_data_template.copy()
         form_data["donation_amount"] = "-5"
-        form_data["owner"] = self.user.account
+        form_data["owner"] = user.account
         form = TaskForm(form_data)
         self.assertEqual(
             form.errors['donation_amount'],
@@ -110,9 +121,10 @@ class TaskTest(TestCase):
 
     def test_valid_donation(self):
         """Tests that tasks with valid time_to_complete pass."""
-        self.user = User.objects.create_user(username='testuser', password='12345')
+        user = User.objects.create_user(
+            username='testuser', password='12345')
         form_data = self.form_data_template.copy()
         form_data["donation_amount"] = "100"
-        form_data["owner"] = self.user.account
+        form_data["owner"] = user.account
         form = TaskForm(form_data)
         self.assertTrue(form.is_valid())
