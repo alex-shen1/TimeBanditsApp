6d0cd3e4f00bfcdc37baf0d2af0f33268338e88c
Dylan Fernandes <dylan.fernandes00@gmail.com>
2020-11-15 20:05:16 -0500

    updated leaderboard with volunteer level and profile links

--- timebandits_app/templatetags/formatters.py
+++ timebandits_app/templatetags/formatters.py
@@ -6,5 +6,9 @@ register = template.Library()
 def currency(dollars):
     dollars = round(float(dollars), 2)
     return "$%s%s" % (intcomma(int(dollars)), ("%0.2f" % dollars)[-3:])
+  
+def volunteer_level(hours):
+    return int(hours / 10) + 1
 
 register.filter('currency', currency)
+register.filter('level', volunteer_level)
