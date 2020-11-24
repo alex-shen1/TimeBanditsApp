d3f765138030f9fee95991435cd9adaa57e231a0
Dylan Fernandes <dylan.fernandes00@gmail.com>
2020-11-23 23:50:56 -0500

    added formatting tests

--- timebandits_app/templatetags/formatters.py
+++ timebandits_app/templatetags/formatters.py
@@ -5,6 +5,16 @@ register = template.Library()
 
 # code inspired by https://stackoverflow.com/questions/346467/format-numbers-in-django-templates
 
+# /***************************************************************************************
+# *  REFERENCES
+# *  Title: Filtering program
+# *  Author: Dave Aaron Smith
+# *  Date: 2/1/2020
+# *  Code version: 1
+# *  URL: https://stackoverflow.com/questions/346467/format-numbers-in-django-templates
+# *  Software License: MIT
+# ***************************************************************************************/
+
 def currency(dollars):
     dollars = round(float(dollars), 2)
     return "$%s%s" % (intcomma(int(dollars)), ("%0.2f" % dollars)[-3:])
